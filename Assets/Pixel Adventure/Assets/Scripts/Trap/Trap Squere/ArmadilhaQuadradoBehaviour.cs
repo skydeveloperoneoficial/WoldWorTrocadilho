898862053d8ca913970fc94f66093bc4c2001532
SkyDeveloper <42675895+skydeveloperoneoficial@users.noreply.github.com>
2024-01-27 17:18:44 -0300

    27.01.24

---
+++ Assets/Pixel Adventure/Assets/Scripts/Trap/Trap Squere/ArmadilhaQuadradoBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//using UnityStandardAssets._2D;
+public class ArmadilhaQuadradoBehaviour : MonoBehaviour
+{
+
+
+    public bool enableGravity;
+    public bool invertGravity;
+    [SerializeField]private Rigidbody2D rd;
+    // Use this for initialization
+    void Start()
+    {
+        enableGravity = false;
+        
+            
+        //rd = FindObjectOfType(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+
+        OnGravity();
+        
+
+    }
+
+    private void OnGravity()
+    {
+        if (enableGravity)
+            rd.gravityScale = 002;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            enableGravity = true;
+            
+            
+        }
+        else
+        {
+            enableGravity = false;
+        }
+
+
+
+
+
+    }
+   
+
+}
---
+++ Assets/Pixel Adventure/Assets/Scripts/Trap/Trap Squere/QuandradoUpBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuandradoUpBehaviour : MonoBehaviour
+{
+    public bool invertGravity,enableGravity;
+    private Rigidbody2D rd;
+    public GameController gameController;
+
+    // Use this for initialization
+    void Start()
+    {
+        rd =  GetComponent<Rigidbody2D>();
+        invertGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InvertGravity();
+    }
+  
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            invertGravity = true;
+            
+           
+
+
+        }
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            gameController.SwitchState(stateMachine.DEAD);
+        }
+    }
+
+    void InvertGravity()
+    {
+        
+        if (invertGravity)
+            rd.gravityScale = -1;
+        
+    }
+   
+}
