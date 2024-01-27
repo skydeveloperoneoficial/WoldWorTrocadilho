f8fd4f6b812db09f2511928569fd33e77c38f47e
pqno07 <pequeno108@gmail.com>
2024-01-27 19:50:31 -0300

    menu

--- Assets/Pixel Adventure/Assets/Scripts/Scenes/Scene.cs
+++ Assets/Pixel Adventure/Assets/Scripts/Scenes/Scene.cs
@@ -7,16 +7,27 @@ public class Scene : MonoBehaviour
      [SerializeField] private string nameScene;
      [SerializeField] private GameObject tela1;
      [SerializeField] private GameObject tela2;
+     [SerializeField] private GameObject music;
+
+    public Button button;
+    public AudioSource audioSource;
 
 
     public string NameScene { get => nameScene; set => nameScene = value; }
+
+    void Start()
+    {
+        // Adiciona um listener para o evento de clique do botão
+        button.onClick.AddListener(PlaySound);
+    }
+
     public void BottonBack()
     {
         ControllerScene.SetScene(nameScene);
     }
     public void SetScene() 
     {
-        
+        music.SetActive(false);
         StartCoroutine(DelayedSetScene());
        
     }
@@ -33,4 +44,15 @@ public class Scene : MonoBehaviour
 
         ControllerScene.SetScene(nameScene);
     }
+
+    void PlaySound()
+    {
+        // Verifica se há um AudioSource associado e um AudioClip
+        if (audioSource && audioSource.clip)
+        {
+            // Reproduz o som
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+    }
+
 }
