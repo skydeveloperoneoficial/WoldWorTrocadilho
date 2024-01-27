134e3d028d615479a57700011fd3cb0ce3d79538
pqno07 <pequeno108@gmail.com>
2024-01-27 19:03:29 -0300

    cutscene pronta

--- Assets/Pixel Adventure/Assets/Scripts/Scenes/Scene.cs
+++ Assets/Pixel Adventure/Assets/Scripts/Scenes/Scene.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
 public class Scene : MonoBehaviour
 {
      [SerializeField] private string nameScene;
-     
+     [SerializeField] private GameObject tela1;
+     [SerializeField] private GameObject tela2;
+
 
     public string NameScene { get => nameScene; set => nameScene = value; }
     public void BottonBack()
@@ -11,6 +16,21 @@ public class Scene : MonoBehaviour
     }
     public void SetScene() 
     {
+        
+        StartCoroutine(DelayedSetScene());
+       
+    }
+
+    IEnumerator DelayedSetScene()
+    {
+        tela1.SetActive(true);
+        
+        yield return new WaitForSeconds(12);
+
+        tela2.SetActive(true);
+
+        yield return new WaitForSeconds(12);
+
         ControllerScene.SetScene(nameScene);
     }
 }
